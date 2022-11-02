5d61e0023b7989a54c5ffae2ba4bfa44329d64aa
huzhengfa <jeremyhzf@gmail.com>
2022-11-02 17:44:49 +0800

    fix:修改主题样式

---
+++ source/js/sticky.js
@@ -0,0 +1,16 @@
+// 人来人往 效果JS
+var mySwiper = new Swiper('#stickyList', {
+    direction: 'horizontal',
+    speed: 600,
+    loop: true,
+    effect : 'fade',
+    loopPreventsSlide: false,
+    autoplay: {
+      delay: 20000,
+    },
+    mousewheel: false,
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+    },
+  })
